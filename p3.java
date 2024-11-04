df506c857ef95a02fc71c45cdab769193b467820
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-11-04 22:35:15 +0530

    typecasting in java

--- p3.java
+++ p3.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class p3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter some input: ");
+        System.out.println("Please enter your roll number: ");
         int rollno = input.nextInt();
+        System.out.println("Enter your marks");
+        float marks = input.nextFloat();
         System.out.println("Your roll no is " + rollno);
+        System.out.println("Your marks is " + marks);
+        
     }
     
 }
---
+++ p4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class p4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        int a = input.nextInt();
+        System.out.println(num);
+
+        //type casting
+        int b = (int)(67.56f);
+        System.out.println(num);
+    }
+    
+}
