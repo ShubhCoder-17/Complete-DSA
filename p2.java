4c7a54d335d55cd5ddc9573e1d52cf52e3be58da
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-11-04 22:00:28 +0530

    Primitive datatypes

--- p2.java
+++ p2.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class p2 {
     public static void main(String[] args) {
-        
+        int rollno = 64;
+        char letter = r;
+        double largeDecimalNumbers = 4567654.4567;
+        long largrInteger = 34567876543L;
+        boolean check = false;
+
     }
     
 }
