6620f62a003ced26c4399a9fcd15a7c7e4dff1bb
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-11-04 22:08:15 +0530

    Input Scanner

---
+++ p3.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class p3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some input: ");
+        int rollno = input.nextInt();
+        System.out.println("Your roll no is " + rollno);
+    }
+    
+}
